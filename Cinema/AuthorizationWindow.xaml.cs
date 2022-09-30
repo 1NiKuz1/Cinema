79a5a0587015aaacca893659f1bfca5d668a1dee
Никита <kaktusloco@gmail.com>
2022-09-30 14:43:39 +0300

    30/09

---
+++ Cinema/AuthorizationWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Cinema
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationWindow.xaml
+    /// </summary>
+    public partial class AuthorizationWindow : Window
+    {
+        public AuthorizationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AuthorizationRollBack_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LoginText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void AuthorizationCommit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RegistrationButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Cinema/SourceCore.cs
@@ -0,0 +1,14 @@
+﻿using Cinema.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema
+{
+    internal class SourceCore
+    {
+        public static cinemaEntities MyBase = new cinemaEntities();
+    }
+}
